de90ce5f3a19ccec81eb45229a4e404d7e663c00
Prieto <Prieto@Prieto>
2024-10-22 10:27:11 +0200

    1.-Creacion del proyecto

---
+++ src/test/java/com/cmepps/listatareas/Sesion5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmepps.listatareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sesion5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
